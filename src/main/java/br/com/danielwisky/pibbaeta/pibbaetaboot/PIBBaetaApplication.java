70137043b1094f5a0d0320c93df1498c907f50bf
Daniel Dias Wisky <daniel-ddw@hotmail.com>
2017-08-07 22:25:58 -0300

    versão inicial do projeto

---
+++ src/main/java/br/com/danielwisky/pibbaeta/pibbaetaboot/PIBBaetaApplication.java
@@ -0,0 +1,12 @@
+package br.com.danielwisky.pibbaeta.pibbaetaboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PIBBaetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PIBBaetaApplication.class, args);
+	}
+}
